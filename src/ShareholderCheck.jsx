dc6335a503cd75930652bbe7f513d48175f73cc7
Apelng <administrator@apelasset.com>
2025-10-16 16:08:32 +0100

    add lasaco agm

--- src/ShareholderCheck.jsx
+++ src/ShareholderCheck.jsx
@@ -37,7 +37,7 @@ const ShareholderCheck = ({ setShareholderData }) => {
     
     try {
       // const API_URL= process.env.REACT_APP_API_URL
-      const response = await fetch(`https://api.lasaco.com.ng/api/check-shareholder`, {
+      const response = await fetch(`https://api.lasaco.apel.com.ng/api/check-shareholder`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ searchTerm }),
@@ -106,7 +106,7 @@ if (editedEmail && !emailRegex.test(editedEmail)) {
         phone_number: updatedShareholder.phone_number,
         chn: updatedShareholder.chn,
       });
-      const response = await fetch('https://api.lasaco.com.ng/api/send-confirmation', {
+      const response = await fetch('https://api.lasaco.apel.com.ng/api/send-confirmation', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
